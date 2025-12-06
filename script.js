e0333ec15532d29f192a1d4e96aacf9ba4661e15
ionutzzzzz <67456260+ionutzzzzz@users.noreply.github.com>
2025-12-06 15:00:07 +0200

    Update timer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Dec 7, 2025 17:20:00").getTime();
+var countDownDate = new Date("Dec 7, 2025 17:00:00").getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
